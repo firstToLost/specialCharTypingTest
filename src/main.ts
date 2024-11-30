242640c7222e53b3774a0a8ac801e5478e857718
Zepar <zepar@Kalo>
2024-11-30 12:58:31 +0300

    basic wpm calculation added

--- src/main.ts
+++ src/main.ts
@@ -41,17 +41,59 @@ function closeGame() {
       <h1 >Start Game</h1>
 `
 }
+function resultShow(sec) {
+  let index = 0
+  const chars = getElement(".words")?.querySelectorAll(".char")
+  chars?.forEach((char, i) => {
+    if (char.classList.contains("current")) index = i
+  })
+  console.log(index)
+  app.innerHTML = `
+      <h1 >${(index
+    ) / sec
+    }
 
+      </h1>
+      <p>${chars.length, Array.from(chars).findIndex(element => element.classList.contains('current'))
+    }<p>
+`
+}
 function PlayGame() {
   const chars = generateChars(500)
   app.innerHTML = `
-    <div class="words-container">
+<div> <div class="timer">00:00</div>    
+<div class="words-container">
       <pre class="words"></pre>
     </div>
+</div>
 `
   const words = getElement(".words")
   chars.forEach(char => {
     words.innerHTML += `<span class="char">${char}</span>`
   });
   words?.querySelectorAll(".char")[0].classList.add("current");
+  startTimer(30)
+}
+function startTimer(seconds: number) {
+  const timerElement = getElement(".timer")
+  let remainingTime = seconds;
+
+  const interval = setInterval(() => {
+    const minutes = Math.floor(remainingTime / 60);
+    const secs = remainingTime % 60;
+
+    timerElement.textContent = `${String(minutes).padStart(2, '0')}:${String(secs).padStart(2, '0')}`;
+    remainingTime--;
+
+    if (remainingTime < 0) {
+      if (playing) {
+        resultShow(seconds)
+        playing = false
+      } else {
+        closeGame()
+      }
+      clearInterval(interval);
+      timerElement.textContent = "Time's Up!";
+    }
+  }, 1000);
 }
